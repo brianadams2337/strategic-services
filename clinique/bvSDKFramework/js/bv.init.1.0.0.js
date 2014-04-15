cb954f3a711b88b7516ee3220a767173e7f67586
Brian Adams <brian@brianadamsdesigns.com>
2014-04-15 15:11:10 -0500

    test

--- clinique/bvSDKFramework/js/bv.init.1.0.0.js
+++ clinique/bvSDKFramework/js/bv.init.1.0.0.js
@@ -127,9 +127,6 @@ function bvLoadSDK () {
 	});
 }
 
-loadScript('http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', function(e) {
-	console.log('google jquery loaded!!!!!!!!!!!!!!!!!!!!!!!!!!');
-})
 loadScript(localPathToSDK + "js/jquery.bvsdk.min." + jqueryVersion + ".js", function() {
 	bvLoadSDK();
 })
